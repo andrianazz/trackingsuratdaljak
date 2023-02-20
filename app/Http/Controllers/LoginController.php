3b1ded5019cdd32816c5d574acc5ee1017db6b74
andrianazz <andrianwahyu41@gmail.com>
2023-02-20 09:49:47 +0700

    testing

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -11,7 +11,7 @@ class LoginController extends Controller
 
     public function index()
     {
-        return view('login');
+        return view('login.index');
     }
 
     public function login(Request $request)
