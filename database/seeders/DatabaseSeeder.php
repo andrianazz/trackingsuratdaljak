84c2c605c31e8842d3ddd67abc5520e38f3e1c84
dhymasriyanto <gojin003@gmail.com>
2023-02-21 14:57:01 +0700

    make seeder  all in once

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([
+        UserSeeder::class,
+        BidangSeeder::class,
+        JenisSuratSeeder::class,
+        SubBidangSeeder::class
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
