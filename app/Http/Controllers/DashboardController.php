270e7799fe7dc996c27589a2736840ffa842b8f3
andrianazz <andrianwahyu41@gmail.com>
2023-02-21 16:07:00 +0700

    fixing tracking

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Surat;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -10,6 +11,7 @@ class DashboardController extends Controller
     public function index()
     {
         $title = "Dashboard";
-        return view('dashboard.index', compact(['title']));
+        $data = Surat::orderBy('tgl_masuk', 'desc')->get();
+        return view('dashboard.index', compact(['title', 'data']));
     }
 }
